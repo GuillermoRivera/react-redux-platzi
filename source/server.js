ff150901d34ab013e90dd5f0d2d48200e6354eb2
Juan Guillermo Rivera Orozco <guillerivera32@gmail.com>
2017-08-19 00:06:10 -0500

    Created server.js file with a basic sintax for render server

---
+++ source/server.js
@@ -0,0 +1,16 @@
+import http from 'http'
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+
+const port = 3000
+
+function requestHandler (req, res) {
+  const html = renderToString(
+    React.DOM.h1(null, 'Hola') // Pasamos null porque no le estamos pasando props
+  )
+  res.write(html) // Escribir en la respuesta el HTML
+  res.end() // Finaliza la respuesta
+}
+
+const server = http.createServer(requestHandler)
+server.listen(port)
