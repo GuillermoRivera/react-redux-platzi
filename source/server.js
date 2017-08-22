3a3014bc57187c5385997c38bf8ac4c73cd5ae95
Juan Guillermo Rivera Orozco <guillerivera32@gmail.com>
2017-08-21 22:34:31 -0500

    Manipulating static routes in server with react-router-dom

--- source/server.js
+++ source/server.js
@@ -1,13 +1,32 @@
 import http from 'http'
 import React from 'react'
-import { renderToString } from 'react-dom/server'
+import { renderToString, renderToStaticMarkup } from 'react-dom/server'
+import { StaticRouter } from 'react-router-dom'
+
+import Pages from './pages/containers/Page.jsx'
+//import Layout from './pages/components/Layout.jsx'
+
 
 const port = 3000
 
 function requestHandler (req, res) {
-  const html = renderToString(
-    React.DOM.h1(null, 'Hola') // Pasamos null porque no le estamos pasando props
+  const context = {}
+  let html = renderToString(
+    //React.DOM.h1(null, 'Hola') // Pasamos null porque no le estamos pasando props
+    <StaticRouter location={req.url} context={context}>
+      <Pages/>
+    </StaticRouter>
   )
+
+  res.setHeader('Content-Type', 'text/html')
+
+  if (context.url) {
+    res.writeHead(301, {
+      Location: context.url
+    })
+    res.end()
+  }
+
   res.write(html) // Escribir en la respuesta el HTML
   res.end() // Finaliza la respuesta
 }
